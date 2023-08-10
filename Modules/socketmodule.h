eaa3bd080ef2299cbe614c348e3b8fdfacb66b48
Mina Galić <freebsd@igalic.co>
2023-08-10 14:24:25 +0000

    GH-107812: extend socket's netlink support to FreeBSD
    for 3.11

--- Modules/socketmodule.h
+++ Modules/socketmodule.h
@@ -89,6 +89,8 @@ typedef int socklen_t;
 #  include <asm/types.h>
 # endif
 # include <linux/netlink.h>
+#elif defined(HAVE_NETLINK_NETLINK_H)
+# include <netlink/netlink.h>
 #else
 #  undef AF_NETLINK
 #endif
